4c57c3488f91efca5e9b335a3d6ef03d6479c82c
ashan012 <khan89891918@outlook.com>
2025-06-19 19:13:23 +0500

    commit

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const appid = "48b19d74e1bdf63f7a93b8ca2ac77569";
 
-let api = async () => {
-  await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${appid}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-    });
+const searchBtn = document.getElementById("searchBtn");
+const inputVal = document.getElementById("inputVal");
+const temprature = document.querySelector(".temprature");
+const description = document.querySelector(".description");
+const windSpeed = document.getElementById("wind-speed");
+const humidity = document.getElementById("humidity");
+const weatherImg = document.getElementById("weatherImg");
+
+let waetherDetails = async (city) => {
+  let data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`
+  );
+  let newData = await data.json();
+
+  temprature.innerHTML = `${Math.round(newData.main.temp - 273.6)} °C`;
+  description.innerHTML = newData.weather[0].main;
+  windSpeed.innerHTML = `${newData.wind.speed} km/h`;
+  humidity.innerHTML = `${newData.main.humidity} %`;
+  switch (newData.weather[0].main) {
+    case "Clouds":
+      weatherImg.src = "../img/cloud.png";
+      break;
+    case "Mist":
+      weatherImg.src = "../img/mist.png";
+      break;
+    case "Rain":
+      weatherImg.src = "../img/rain.png";
+      break;
+    case "Clear":
+      weatherImg.src = "../img/clear.png";
+      break;
+    case "Snow":
+      weatherImg.src = "../img/snow.png";
+      break;
+  }
 };
-api();
+searchBtn.addEventListener("click", () => {
+  waetherDetails(inputVal.value);
+});
