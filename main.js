08d58137603021d186987d7454ac0e51b94323a8
ashan012 <khan89891918@outlook.com>
2025-06-20 19:17:40 +0500

    commit

--- main.js
+++ main.js
@@ -7,33 +7,43 @@ const description = document.querySelector(".description");
 const windSpeed = document.getElementById("wind-speed");
 const humidity = document.getElementById("humidity");
 const weatherImg = document.getElementById("weatherImg");
+const weatherBody = document.querySelector(".weather-body");
+
+const locationNotFound = document.querySelector(".location-not-found");
 
 let waetherDetails = async (city) => {
   let data = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`
   );
   let newData = await data.json();
-
-  temprature.innerHTML = `${Math.round(newData.main.temp - 273.6)} °C`;
-  description.innerHTML = newData.weather[0].main;
-  windSpeed.innerHTML = `${newData.wind.speed} km/h`;
-  humidity.innerHTML = `${newData.main.humidity} %`;
-  switch (newData.weather[0].main) {
-    case "Clouds":
-      weatherImg.src = "../img/cloud.png";
-      break;
-    case "Mist":
-      weatherImg.src = "../img/mist.png";
-      break;
-    case "Rain":
-      weatherImg.src = "../img/rain.png";
-      break;
-    case "Clear":
-      weatherImg.src = "../img/clear.png";
-      break;
-    case "Snow":
-      weatherImg.src = "../img/snow.png";
-      break;
+  try {
+    locationNotFound.style.display = "none";
+    weatherBody.style.display = "flex";
+    temprature.innerHTML = `${Math.round(newData.main.temp - 273.6)} °C`;
+    description.innerHTML = newData.weather[0].main;
+    windSpeed.innerHTML = `${newData.wind.speed} km/h`;
+    humidity.innerHTML = `${newData.main.humidity} %`;
+    switch (newData.weather[0].main) {
+      case "Clouds":
+        weatherImg.src = "../img/cloud.png";
+        break;
+      case "Mist":
+        weatherImg.src = "../img/mist.png";
+        break;
+      case "Rain":
+        weatherImg.src = "../img/rain.png";
+        break;
+      case "Clear":
+        weatherImg.src = "../img/clear.png";
+        break;
+      case "Snow":
+        weatherImg.src = "../img/snow.png";
+        break;
+    }
+  } catch (error) {
+    weatherBody.style.display = "none";
+    locationNotFound.style.display = "flex";
+    console.error(error);
   }
 };
 searchBtn.addEventListener("click", () => {
